3445d22e011ab54136aee055d833697493d55c57
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2020-04-05 13:16:01 +0100

    changed to SMbus commands

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # VishayVCNL4035_Test
 Vishay Gesture sensor VCNL4035X01 test
+
+This programm uses linux system calls to access I2C bus from linux user space. System calls are how a programm enters the kernel to perform some task
+x86-64 CPUs have concept called privilege levels. The kernel runs at the most privileged level, called “Ring 0”. User programs run at a lesser level, typically “Ring 3”.
+To run this code on raspberry:
+1.enable I2C (sudo raspi-config);
+2.sudo apt-get install i2c-tools
+3.sudo apt-get update
+4.sudo apt-get install libi2c-dev
+
+You can test I2C bus:
+>ls /dev/*i2c* 
+sudo i2cdetect -y x //search for all I2C devices on bus and returns addresses. x= 0 or 1. 
+sudo i2cget -y x [device addr] [register addr] 
+
+
+
+
+Useful links:
+https://www.kernel.org/doc/Documentation/i2c/dev-interface
+https://elinux.org/Interfacing_with_I2C_Devices
+https://raspberry-projects.com/pi/programming-in-c/i2c/using-the-i2c-interface
--- main.c
+++ main.c
@@ -1,107 +1,77 @@
-#include <unistd.h>				//Needed for I2C port
-#include <fcntl.h>				//Needed for I2C port
-#include <sys/ioctl.h>			//Needed for I2C port
-#include <linux/i2c-dev.h>		//Needed for I2C port
-#include "vcnl4035.h"
+/*
+ * Simple I2C example
+ *
+ * Copyright 2017 Joel Stanley <joel@jms.id.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
 
-#define BIT(n) 1u<<n
-#define DEV_ADDR 0x60
-static void i2c_write(uint8_t,uint16_t);
-int file_i2c; 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <err.h>
+#include <errno.h>
 
-enum vcnl4035_regs {
-	ALS_CONF = 0,	// 0x0
-	ALS_THDH,	// 0x1
-	ALS_THDL,	// 0x2
-	PS_CONF,	// 0x3
-	PS_CONF_MS,	// 0x4
-	PS_CANC,	// 0x5
-	PS_THDL,	// 0x6
-	PS_THDH,	// 0x7
-	PS1_DATA,	// 0x8
-	PS2_DATA,	// 0x9
-	PS3_DATA,	// 0xa
-	ALS_DATA,	// 0xb
-	WHITE_DATA,	// 0xc
-	INT_FLAG,	// 0xd
-	ID,		// 0xe
-};
+#include <linux/types.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command,
+                                     int size, union i2c_smbus_data *data)
+{
+	struct i2c_smbus_ioctl_data args;
+
+	args.read_write = read_write;
+	args.command = command;
+	args.size = size;
+	args.data = data;
+	return ioctl(file,I2C_SMBUS,&args);
+}
+
+
+static inline __s32 i2c_smbus_read_word_data(int file, __u8 command)
+{
+	union i2c_smbus_data data;
+	uint16_t i2c_data;
+	if (i2c_smbus_access(file,I2C_SMBUS_READ,command,
+	                     I2C_SMBUS_WORD_DATA,&data))
+		return -1;
+	else
+	{
+
+	return data.word;
+}
+}
 
 int main()
 {
-	
-	int length;
-	unsigned char buffer[60] = {0};
+	uint8_t  addr = 0x60, reg[0xf] ={0};// 0xe;
+	uint16_t data=0;
+	const char *path = "/dev/i2c-1";
+	int file, rc;
 
 	
-	//----- OPEN THE I2C BUS -----
-	char *filename = (char*)"/dev/i2c-1";
-	if ((file_i2c = open(filename, O_RDWR)) < 0)
-	{
-		//ERROR HANDLING: you can check errno to see what went wrong
-		printf("Failed to open the i2c bus");
-		return;
-	}
-	
-	int addr = 0x60;          //<<<<<The I2C address of the slave
-	if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
-	{
-		printf("Failed to acquire bus access and/or talk to slave.\n");
-		//ERROR HANDLING; you can check errno to see what went wrong
-		return;
-	}ok, 
-	
-	
-	//----- READ BYTES -----
-	length = 4;			//<<< Number of bytes to read
-	if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		//ERROR HANDLING: i2c transaction failed
-		printf("Failed to read from the i2c bus.\n");
-	}
-	else
-	{
-		printf("Data read: %s\n", buffer);
-	}        
 
-	
-	//----- WRITE BYTES -----
-	buffer[0] = 0x01;
-	buffer[1] = 0x02;
-	length = 2;			//<<< Number of bytes to write
-	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		/* ERROR HANDLING: i2c transaction failed */
-		printf("Failed to write to the i2c bus.\n");
-	}
-	
-	
-	i2c_write( PS_CONF, BIT(14) | BIT(15));
-	i2c_write( PS_CONF_MS, BIT(0) | BIT(3));
+	file = open(path, O_RDWR);
+	if (file < 0)
+		err(errno, "Tried to open '%s'", path); 
 
-	i2c_write( PS_CONF_MS, BITL(3) | BITH(0));
-	i2c_write( PS_CONF, (4 << 1) | (1 << 6) | (2 << 8) 
-			| BITH(2) | BITH(3) | BITH(6));
-	i2c_write(ALS_CONF, (2 << 5));
-	i2c_write(PS_THDL, 0x1ff);
-	
-	
-	
-	
-	
-return 1;
-}
+	rc = ioctl(file, I2C_SLAVE, addr);
+	if (rc < 0)
+		err(errno, "Tried to set device address '0x%02x'", addr);
 
-
-static void i2c_write(uint8_t reg,uint16_t value)
+for(int i =0; i<0xf;i++)
 {
-	uint8_t dev_addr=DEV_ADDR;
-uint8_t buff[4] = {dev_addr,reg,value & 0xff,value>>8};
-
-if (write(file_i2c, buff, 4) != 4)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		/* ERROR HANDLING: i2c transaction failed */
-		printf("Failed to write to the i2c bus.\n");
-	}
+	
+	printf("%02x:0x%04x\n",i,i2c_smbus_read_word_data(file,i));
 }
 
+}
